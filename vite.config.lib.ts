6001b3acfb20059b969d748113fb31b43dde4f40
Jonathan Kurinsky <jkurinsky@tubi.tv>
2023-01-05 18:45:16 -0500

    add coverage reporting

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   base: './',
@@ -11,6 +11,11 @@ export default defineConfig({
       name: 'mylib',
       formats: ['es', 'cjs', 'umd', 'iife'],
       fileName: (format) => `index.${format}.js`,
+    },
+  },
+  test: {
+    coverage: {
+      provider: 'istanbul',
     },
   },
 });

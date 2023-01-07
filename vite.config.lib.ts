556d3ecb87039851a8e0dfcd92aa325acf145120
Jonathan Kurinsky <jkurinsky@tubi.tv>
2023-01-06 21:09:56 -0500

    add coverage threshholds

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -17,6 +17,10 @@ export default defineConfig({
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'json-summary', 'html'],
+      lines: 100,
+      functions: 100,
+      branches: 100,
+      statements: 100,
     },
   },
 });
